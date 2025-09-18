8397c3a522a571bf758781d78b045300d5fff8c6
Joaquin <124814537+Joacs49@users.noreply.github.com>
2025-09-18 13:57:00 -0500

    Apartado de inicio del blog realizado (#3)
    * Se añadio breeze y los controladores con migraciones
    
    * Creacion de aside y rutas
    
    * Estructura de inicio de blog acabada

---
+++ resources/views/components/blog/post.blade.php
@@ -0,0 +1,29 @@
+<section class="max-w-md w-full flex flex-col border p-4 mb-4">
+    <div class="flex flex-row items-center mb-4">
+        <img src="" alt="" class="w-12 h-12 rounded-full mr-4">
+        <div class="flex flex-col">
+            <p>María García</p>
+            <p>Fecha</p>
+        </div>
+    </div>
+
+    <div>
+        <h3>Introducción a Next.js 15: Las nuevas características que debes conocer</h3>
+    </div>
+
+    <div>
+        <p>Descubre las últimas funcionalidades de Next.js 15 y cómo pueden mejorar tu flujo de desarrollo web moderno.</p>
+    </div>
+
+    <div class="flex flex-row gap-4 mb-4">
+        <p>Next.js</p>
+        <p>React</p>
+        <p>Web Development</p>
+    </div>
+
+    <div class="flex flex-row gap-4 mb-4">
+        <p>Interacciones</p>
+        <p>Interacciones</p>
+        <p>Interacciones</p>
+    </div>
+</section>
