4026933dc85c6ece0c3b862d17c81fd3441b67cd
Joaquin <124814537+Joacs49@users.noreply.github.com>
2025-09-24 20:08:52 -0500

    Trayendo las estructuras realizas (#4)
    * Se añadio breeze y los controladores con migraciones
    
    * Creacion de aside y rutas
    
    * Estructura de inicio de blog acabada
    
    * Estructura del apartado de posts listo
    
    * Estructura de escribir un blog realizado

--- resources/views/blog/blogList.blade.php
+++ resources/views/blog/blogList.blade.php
@@ -4,6 +4,27 @@
     @section('content')
         <div class="p-8">
             <h1 class="text-2xl font-bold mb-4">Listado de Post</h1>
-            <!-- Tu contenido aquí -->
+            <h2>Explora nuestra colección de artículos sobre desarrollo web y tecnología</h2>
         </div>
+
+        <div class="pl-8">
+            <input type="text" name="search" id="search">
+        </div>
+
+        <section class="pt-8 pl-8">
+            <div class="flex gap-10">
+                <x-blog.post class="w-1/3" />
+                <x-blog.post class="w-1/3" />
+                <x-blog.post class="w-1/3" />
+            </div>
+        </section>
+
+        <section class="pt-8 pl-8">
+            <div class="flex flex-row justify-center items-center gap-5">
+                <button>< Anterior</button>
+                <button>1</button>
+                <button>2</button>
+                <button>Siguiente ></button>
+            </div>
+        </section>
     @endsection
